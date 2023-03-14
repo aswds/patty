b0cba04eecca22249d26a83bc9b86ebe3aca475f
aswds <sasha.iilllnitskiy@gmail.com>
2023-03-14 22:04:15 +0200

    Added loader

---
+++ app/shared/Loaders/EventsLoader.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+import { ActivityIndicator, Text, View } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { colors } from "../../src/colors";
+import { FontFamily } from "../../../assets/fonts/Fonts";
+
+type EventLoaderProps = {
+  isLoading: boolean;
+};
+const EventLoader = ({ isLoading }: EventLoaderProps) => {
+  const insets = useSafeAreaInsets();
+  return (
+    <View
+      style={{
+        position: "absolute",
+        backgroundColor: colors.modalBackground,
+        zIndex: 1,
+        borderTopRightRadius: 10,
+        borderBottomRightRadius: 10,
+        top: insets.top + 10,
+        padding: 10,
+        flexDirection: "row",
+      }}
+    >
+      <Text
+        style={{ color: colors.accentColor, fontFamily: FontFamily.medium }}
+      >
+        Loading events in your city
+      </Text>
+      <ActivityIndicator
+        color={colors.accentColor}
+        size={"small"}
+        style={{ marginHorizontal: 10 }}
+      />
+    </View>
+  );
+};
+
+export default EventLoader;
