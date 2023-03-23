21c4082c95ef09aec24a259b5b2e8bcb8227a6ec
aswds <sasha.iilllnitskiy@gmail.com>
2023-03-23 18:44:43 +0200

    🐛 fix(Events.ts): fix import paths for fetchEventsByCity and IUser ✨ feat(Events.ts): add city parameter to fetch_joined_events function and filter events by user's joined events in that city
    🐛 fix(User.ts): replace getDoc with onSnapshot to listen for changes in user document
    ✨ feat(User.ts): add updateUser reducer to update current_user state in store

--- app/redux/actions/User.ts
+++ app/redux/actions/User.ts
@@ -1,4 +1,4 @@
-import { doc, getDoc } from "firebase/firestore";
+import { doc, onSnapshot } from "firebase/firestore";
 import { Alert } from "react-native";
 import { auth, db } from "../../../firebase";
 import { createAsyncThunk } from "@reduxjs/toolkit";
@@ -6,25 +6,22 @@ import { IUser } from "../../Types/User";
 
 export const fetch_user = createAsyncThunk(
   "user/fetch_user",
-  async (): Promise<IUser | Error> => {
-    try {
+  async (): Promise<IUser> => {
+    return new Promise((resolve, reject) => {
       const docRef = doc(db, `USERS/${auth.currentUser?.uid}`);
-      const docSnap = await getDoc(docRef);
-
-      if (docSnap.exists()) {
-        return docSnap.data() as IUser;
-      } else {
-        Alert.alert(
-          "User not found",
-          "We can't find user with that credentials."
-        );
-        await auth.signOut();
-        return new Error();
-      }
-    } catch (err) {
-      Alert.alert("We can't load user with that credentials.");
-      await auth.signOut();
-      return new Error();
-    }
+      onSnapshot(docRef, async (docSnap) => {
+        if (docSnap.exists()) {
+          resolve(docSnap.data() as IUser);
+          // eventEmitter.emit("user_change");
+        } else {
+          Alert.alert(
+            "User not found",
+            "We can't find user with that credentials."
+          );
+          await auth.signOut();
+          reject(new Error());
+        }
+      });
+    });
   }
 );
