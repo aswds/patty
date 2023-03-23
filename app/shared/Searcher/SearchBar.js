ddcc26818c21cc08f1dfc90481d3023e144ca299
aswds <sasha.iilllnitskiy@gmail.com>
2023-03-23 18:45:27 +0200

    🔥 refactor(UserInfo.tsx): remove console.log statement 🐛 fix(getAddress.ts): update HERE API endpoint and resolve promise with correct data 🔧 chore(SearchBar.js): fix formatting and spacing
    🐛 fix(UserList.tsx): fix typo in renderItem prop, remove unnecessary underscore character

--- app/shared/Searcher/SearchBar.js
+++ app/shared/Searcher/SearchBar.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {StyleSheet, TextInput, View} from "react-native";
+import { StyleSheet, TextInput, View } from "react-native";
 
 export default function SearchBar({
   placeholderText,
