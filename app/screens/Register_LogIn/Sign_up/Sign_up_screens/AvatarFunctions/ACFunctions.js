74f51722f63b3ef2f9224704b272489afc708cb4
aswds <sasha.iilllnitskiy@gmail.com>
2023-03-25 00:59:43 +0200

    Changed/Deleted files

--- app/screens/Register_LogIn/Sign_up/Sign_up_screens/AvatarFunctions/ACFunctions.js
+++
@@ -1,6 +0,0 @@
-export const _hideModal = (setShowModal) => {
-  setShowModal(false);
-};
-export const _imagePropHandler = (setImage, imageProp) => {
-  setImage(imageProp);
-};
--- app/screens/Register_LogIn/Sign_up/Sign_up_screens/Sign_up_Functions/_showModalHandel.js
+++
@@ -1,7 +0,0 @@
-import { isAndroid } from "../../../../../src/platform";
-import { IOSModal } from "../IOSModal";
-
-export const _showModalHandle = (setImage, setShowModal) => {
-  !isAndroid && IOSModal(setImage);
-  setShowModal(isAndroid);
-};
