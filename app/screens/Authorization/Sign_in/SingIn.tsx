763f1ecd62ec57e8ab76eb73bdc847e4ea1d24de
aswds <sasha.iilllnitskiy@gmail.com>
2023-03-25 00:58:00 +0200

    ✨ feat(SingIn.tsx): add SignInScreen component with email and password inputs, forgot password button, and sign in button.
    ✨ feat(SignUp.tsx): add SignUp screen with form inputs and validation logic
    
    ✨ feat(AvatarChoose.tsx): add AvatarChoose screen for user to choose their avatar image during sign up process
    
    ✨ feat(NameModal.tsx): add NameModal component for sign up process
    
    ✨ feat(Username.tsx): add Username screen for sign up process with input validation and navigation to next screen.

---
+++ app/screens/Authorization/Sign_in/SingIn.tsx
@@ -0,0 +1,118 @@
+import { Feather, FontAwesome } from "@expo/vector-icons";
+import React, { useState } from "react";
+import { Keyboard, View } from "react-native";
+import { colors } from "../../../src/colors";
+import { BackButton } from "../../../shared/Buttons/BackButton";
+import Button from "../components/button";
+import Input from "../../../shared/Input/Input";
+import { Logo } from "../components/Logo";
+import { Screen } from "../components/Screen";
+import CustomAlert from "../CustomAlert";
+import { textStyle } from "../style";
+import ForgotPassword from "./SignInComponents/ForgotPassword";
+import { user_signIn } from "./SignInComponents/SignInFuncs/signInFunction";
+import { styles } from "./styles";
+import { SignInScreenNavigationProps } from "../../../Types/Authorization/SignIn/ScreenNavigationProps";
+
+const SignInScreen = ({ navigation }: SignInScreenNavigationProps) => {
+  const [email, setEmail] = useState({ isValid: true, errorMsg: "" });
+  const [password, setPassword] = useState({ isValid: true, errorMsg: "" });
+  const [errorMsg, setErrorMsg] = useState<string>();
+  const [userPassword, setUserPassword] = useState<string>("");
+  const [userLogin, setUserLogin] = useState<string>("");
+  const [showPassword, setShowPassword] = useState<boolean>(true);
+  const [_showModal, setShowModal] = useState<boolean>(false);
+
+  const _hideModal = () => {
+    setShowModal(false);
+  };
+
+  const _clearMsg = () => {
+    setErrorMsg("");
+  };
+  const iconSize = 24;
+  const UserIcon = (
+    <FontAwesome name="user-o" size={iconSize} color={colors.iconColor} />
+  );
+  const PasswordIcon = (
+    <Feather
+      name={showPassword ? "eye-off" : "eye"}
+      size={iconSize}
+      color={colors.iconColor}
+      onPress={() => setShowPassword(!showPassword)}
+    />
+  );
+
+  return (
+    <>
+      <Screen styles={styles} keyboardDismiss={Keyboard.dismiss}>
+        <BackButton navigation={navigation} />
+        <View style={styles.logoContainer}>
+          <Logo />
+        </View>
+        <View style={styles.loginContainer}>
+          <View style={styles.innerText}>
+            <Input
+              isValid={email.isValid}
+              icon={UserIcon}
+              inputStyle={styles.inputField}
+              keyboardType="email-address"
+              placeholderTextColor={textStyle.color}
+              placeholder="Enter e-mail"
+              onChangeText={(email: string) => {
+                setUserLogin(email);
+                setEmail({ errorMsg: "", isValid: true });
+              }}
+              defaultValue={userLogin}
+              autoCapitalize="none"
+              style={{ height: 60 }}
+            />
+
+            <Input
+              isValid={password.isValid}
+              icon={PasswordIcon}
+              secureTextEntry={showPassword}
+              placeholder="Password"
+              placeholderTextColor={textStyle.color}
+              onChangeText={(password) => {
+                setUserPassword(password);
+                setPassword({ errorMsg: "", isValid: true });
+              }}
+              defaultValue={userPassword}
+              inputStyle={styles.inputField}
+              style={{ height: 60 }}
+            />
+            <ForgotPassword styles={styles} navigation={navigation} />
+          </View>
+        </View>
+        <View style={styles.styledButtonContainer}>
+          <Button
+            onPress={() => {
+              user_signIn(
+                setEmail,
+                setPassword,
+                setErrorMsg,
+                setShowModal,
+                userLogin,
+                userPassword
+              );
+            }}
+            style={styles.styledButtonStyle}
+            textStyle={styles.styledButtonTextStyle}
+          >
+            Sign in
+          </Button>
+        </View>
+      </Screen>
+
+      <CustomAlert
+        errorMsg={errorMsg}
+        hideModal={_hideModal}
+        showModal={_showModal}
+        clearMsg={_clearMsg}
+      />
+    </>
+  );
+};
+
+export default SignInScreen;
---
+++ app/screens/Authorization/Sign_up/SignUp.tsx
@@ -0,0 +1,284 @@
+import {
+  Feather,
+  MaterialCommunityIcons,
+  MaterialIcons,
+} from "@expo/vector-icons";
+import { useRoute } from "@react-navigation/native";
+import React, { MutableRefObject, useRef, useState } from "react";
+import { Dimensions, StyleSheet, TextInput, View } from "react-native";
+import { colors } from "../../../src/colors";
+import { isAndroid } from "../../../src/platform";
+import { BackButton } from "../../../shared/Buttons/BackButton";
+import Button from "../components/button";
+import Input from "../../../shared/Input/Input";
+import { Logo } from "../components/Logo";
+import { Screen } from "../components/Screen";
+import CustomAlert from "../CustomAlert";
+import { textStyle } from "../style";
+import { Container } from "./Sign_up_components/Container";
+import { TermText } from "./Sign_up_components/TermText";
+import { error_handle } from "./Sign_up_screens/Sign_up_Functions/error_handle";
+import { signUpHandle } from "./Sign_up_screens/Sign_up_Functions/signUp";
+import { FontFamily } from "../../../../assets/fonts/Fonts";
+import { SignUpRouteProps } from "../../../Types/Authorization/SignUp/RouteTypes";
+import { SignUpNavigationProps } from "../../../Types/Authorization/SignUp/ScreenNavigationProps";
+
+type SignUpUser = {
+  email: string | null;
+  password: string | null;
+};
+
+const SignUpScreen = ({ navigation }: SignUpNavigationProps) => {
+  const [valid, setValid] = useState({
+    validUsername: true,
+    validEmail: true,
+    validPassword: true,
+    validConfirmPassword: true,
+  });
+  const [user, setUser] = useState<SignUpUser>({
+    email: null,
+    password: null,
+  });
+  const [showModal, setShowModal] = useState<boolean>(false);
+  const [confirmPass, setConfirmPass] = useState<string>("");
+  const [showPassword, setShowPassword] = useState<boolean>(true);
+  const [errorMsg, setErrorMsg] = useState<string>();
+  const ref_input2 = useRef<TextInput>(null);
+  const ref_input3 = useRef<TextInput>(null);
+  const ref_input4 = useRef<TextInput>(null);
+  const route = useRoute<SignUpRouteProps>();
+  const name = route.params?.name;
+  const surname = route.params?.surname;
+  const username = route.params?.username;
+  const image = route.params?.imageURI;
+
+  const _hideModal = () => {
+    setShowModal(false);
+  };
+  function signUp_handle() {
+    if (
+      valid.validEmail &&
+      valid.validPassword &&
+      user.password === confirmPass &&
+      user.email
+    ) {
+      //handling sign up
+      signUpHandle(
+        user.email.trim(),
+        user.password.trim(),
+        username,
+        name,
+        surname,
+        image
+      ).catch((err) => {
+        //if error occurs, show modal
+        error_handle(err.error_type, err.message, {
+          setValid,
+          valid,
+        }).catch((err) => {
+          setShowModal(true);
+          setErrorMsg(err);
+        });
+      });
+    } else {
+      setShowModal(true);
+      setErrorMsg("Please check if everything is correct :)");
+    }
+  }
+  function refHandle(ref_input: MutableRefObject<TextInput | null>) {
+    ref_input?.current?.focus();
+  }
+  function PasswordIcon() {
+    return showPassword ? (
+      <Feather
+        name="eye-off"
+        size={Dimensions.get("window").height >= 800 ? 24 : 20}
+        color={colors.iconColor}
+        onPress={() => setShowPassword(false)}
+      />
+    ) : (
+      <Feather
+        name="eye"
+        size={Dimensions.get("window").height >= 800 ? 24 : 20}
+        color={colors.iconColor}
+        onPress={() => setShowPassword(true)}
+      />
+    );
+  }
+
+  return (
+    <Screen>
+      <View>
+        <Logo />
+      </View>
+      <BackButton navigation={navigation} />
+
+      <Container>
+        <Input
+          style={styles.inputStyle}
+          icon={
+            <MaterialIcons
+              name="alternate-email"
+              size={Dimensions.get("window").height >= 800 ? 24 : 20}
+              color={colors.iconColor}
+            />
+          }
+          isValid={valid.validEmail}
+          autoCapitalize="none"
+          keyboardType="email-address"
+          placeholder="Email"
+          placeholderTextColor={textStyle.color}
+          onSubmitEditing={() => {
+            refHandle(ref_input3);
+          }}
+          onChangeText={(text) => {
+            setUser({ ...user, email: text });
+            setValid({ ...valid, validEmail: true });
+          }}
+          value={user.email!}
+          ref={ref_input2}
+          inputStyle={{
+            ...styles.inputField,
+          }}
+        />
+
+        <Input
+          isValid={valid.validPassword}
+          style={styles.inputStyle}
+          icon={<PasswordIcon />}
+          secureTextEntry={showPassword}
+          placeholder="Password"
+          placeholderTextColor={textStyle.color}
+          onChange={() => {
+            setValid({ ...valid, validPassword: true });
+          }}
+          onChangeText={(text) => {
+            setUser({ ...user, password: text });
+            // checkPassword(text, setValid, setPasswordError);
+          }}
+          onSubmitEditing={() => {
+            refHandle(ref_input4);
+          }}
+          value={user.password!}
+          ref={ref_input3}
+          inputStyle={styles.inputField}
+        />
+
+        <Input
+          style={styles.inputStyle}
+          isValid={valid.validConfirmPassword}
+          icon={
+            <MaterialCommunityIcons
+              name="lock-check"
+              size={Dimensions.get("window").height >= 800 ? 24 : 20}
+              color={colors.iconColor}
+            />
+          }
+          autoCapitalize="none"
+          secureTextEntry={showPassword}
+          placeholder="Confirm your password"
+          placeholderTextColor={textStyle.color}
+          onChangeText={(text) => {
+            setConfirmPass(text);
+            if (user.password !== text) {
+              setValid({ ...valid, validConfirmPassword: false });
+            } else {
+              setValid({ ...valid, validConfirmPassword: true });
+            }
+          }}
+          defaultValue={confirmPass}
+          ref={ref_input4}
+          inputStyle={styles.inputField}
+        />
+      </Container>
+      <Button
+        textStyle={{
+          fontFamily: "Nunito-Bold",
+          fontSize: 20,
+          color: "#E7E0C9",
+        }}
+        style={styles.styledButton}
+        onPress={signUp_handle}
+      >
+        Sign Up
+      </Button>
+      {/* Fix */}
+      <TermText />
+      {/* Fix */}
+      <CustomAlert
+        errorMsg={errorMsg}
+        hideModal={_hideModal}
+        showModal={showModal}
+        setErrorMsg={setErrorMsg}
+      />
+    </Screen>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  inputStyle: {
+    width: "100%",
+    height: isAndroid ? Dimensions.get("window").height * 0.1 : "20%",
+  },
+  errorMsg: {
+    color: "red",
+  },
+  registerContainer: {
+    marginTop: "5%",
+    height: Dimensions.get("window").height / 2.2,
+    width: Dimensions.get("window").width / 1.3,
+    justifyContent: "center",
+  },
+
+  textContainer: {
+    justifyContent: "center",
+    paddingBottom: 10,
+  },
+  text: {
+    fontSize: 15,
+    fontWeight: "bold",
+    fontVariant: ["small-caps"],
+  },
+
+  inputField: {
+    color: "white",
+    width: "100%",
+    height: "100%",
+    justifyContent: "center",
+    borderRadius: 10,
+    fontSize: 13,
+    fontFamily: FontFamily.medium,
+  },
+  image: {
+    height: "100%",
+    width: "100%",
+  },
+  imageContainer: {
+    marginTop: 20,
+    height: 80,
+    width: 75,
+    shadowOpacity: 0.4,
+    shadowOffset: { height: 2, width: 0 },
+  },
+
+  animationStyle: {
+    width: Dimensions.get("window").width / 1.5,
+    justifyContent: "center",
+    padding: 10,
+  },
+  styledButton: {
+    marginBottom: 20,
+    borderRadius: 13,
+    backgroundColor: "rgba(155 , 50, 50 , 1)",
+    height: isAndroid ? "10%" : 70,
+    width: "80%",
+    alignSelf: "center",
+  },
+});
+
+export default SignUpScreen;
