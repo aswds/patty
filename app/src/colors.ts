3111e0d647b775cc600328472b65a75ad3e84047
aswds <sasha.iilllnitskiy@gmail.com>
2023-04-07 03:08:52 +0300

    🎨 style(colors.ts): add support for dark and light theme 🎨 style(theme.ts): add isDarkTheme variable to toggle between dark and light theme

--- app/src/colors.ts
+++ app/src/colors.ts
@@ -1,18 +1,20 @@
+import { isDarkTheme } from "./theme";
+
 export const colors = {
-  background: "#202020", //#202020
-  text: "white",
-  text_2: "#AEAEAE",
-  accentColor: "#9B3232", // #9B3232  rgba(155 , 50, 50 , 1)
-  disabledButton: "#5F3937",
-  disabledText: "#947A7A",
-  input: "#2C2C2C",
-  iconColor: "#6D6D6D",
-  buttonTextColor: "#E7E0C9",
+  background: isDarkTheme ? "#202020" : "#FFFFFF", //#202020
+  text: isDarkTheme ? "white" : "#2E2E2E",
+  text_2: isDarkTheme ? "#AEAEAE" : "#404040",
+  accentColor: isDarkTheme ? "#9B3232" : "rgba(255, 80, 80, 1)", // #9B3232  rgba(155 , 50, 50 , 1)
+  disabledButton: isDarkTheme ? "#5F3937" : "#E88B86",
+  disabledText: isDarkTheme ? "#947A7A" : "#F2C8C8",
+  input: isDarkTheme ? "#2C2C2C" : "#F2F2F2",
+  iconColor: isDarkTheme ? "#6D6D6D" : "#9d9d9d",
+  buttonTextColor: "white",
   mapAccentColor: "rgb(210, 40, 45)", //#D2282D
-  doneButtonBG: "#2B3526", // Profile screen // Edit button
-  doneButtonText: "#9FC084", // Profile screen // Edit button
-  buttonBG: "#2F2524", // Profile screen // Edit button
-  buttonText: "#FF5454", // Profile screen // Edit button
+  doneButtonBG: isDarkTheme ? "#2B3526" : "#9CC08A", // Profile screen // Edit button
+  doneButtonText: isDarkTheme ? "#9FC084" : "white", // Profile screen // Edit button
+  buttonBG: isDarkTheme ? "rgba(47, 37, 36, 1)" : "rgba(109, 109, 109, 0.1)", // rgba(47, 37, 36,) Profile screen // Edit button
+  buttonText: isDarkTheme ? "rgba(255, 84, 84)" : "black", // Profile screen // Edit button
   cancel: "#A85959", //TypeError
   blue_text: "#0A67FF",
   partyCreateScreenColors: {
@@ -25,5 +27,10 @@ export const colors = {
     midGroupOfPeople: "#D04848",
     bigGroupOfPeople: "#FF0000",
   },
-  modalBackground: "#1c1c1c",
+  initialScreenButton: isDarkTheme ? "#363636" : "#F6F6F6",
+  modalBackground: isDarkTheme ? "#1c1c1c" : "#FFFFFF",
+};
+
+const lightColor = {
+  primary: "#FFD700",
 };
