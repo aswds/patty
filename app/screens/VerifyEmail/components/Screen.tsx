c65cb9704c74f3b055ad048e4c4fcb1b1595fbea
aswds <sasha.iilllnitskiy@gmail.com>
2023-04-26 17:48:53 +0300

    Slight changes

---
+++ app/screens/VerifyEmail/components/Screen.tsx
@@ -0,0 +1,44 @@
+import React, { PropsWithChildren } from "react";
+import {
+  Keyboard,
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+  StyleSheet,
+  TouchableWithoutFeedback,
+  View,
+} from "react-native";
+import { colors } from "../../../src/colors";
+
+interface ScreenProps extends PropsWithChildren {}
+
+const Screen = ({ children }: ScreenProps) => {
+  return (
+    <ScrollView
+      style={styles.container}
+      contentContainerStyle={{ flexGrow: 1 }}
+      keyboardShouldPersistTaps="handled"
+    >
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        style={styles.keyboardContainer}
+      >
+        {children}
+      </KeyboardAvoidingView>
+    </ScrollView>
+  );
+};
+const styles = StyleSheet.create({
+  keyboardContainer: {
+    flex: 1,
+    justifyContent: "center",
+  },
+
+  container: {
+    flex: 1,
+    width: undefined,
+    height: undefined,
+    backgroundColor: colors.background,
+  },
+});
+export default Screen;
---
+++ writeJson.js
@@ -0,0 +1,26 @@
+const fs = require("fs");
+
+// Read the contents of the text file
+fs.readFile("data.txt", "utf8", (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+
+  // Split the text into an array of words
+  const words = data.split("\n");
+
+  // Create an array of objects from the words
+
+  // Convert the array of objects into a JSON string
+  const jsonString = JSON.stringify(words, null, 2);
+
+  // Write the JSON string to a file
+  fs.writeFile("data.json", jsonString, (err) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.log("Data saved to data.json");
+  });
+});
