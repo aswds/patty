bb2ec3e640934c83fe28d2192e423cefe3dc56d6
aswds <sasha.iilllnitskiy@gmail.com>
2023-05-06 19:19:21 +0300

    🎨 style(pickColor.ts): refactor pickColor function to accept partyMarkerColors object ✨ feat(pickColor.ts): add support for custom event marker colors through partyMarkerColors object

--- app/screens/Map/pickColor.ts
+++ app/screens/Map/pickColor.ts
@@ -1,11 +1,15 @@
-import {colors} from "../../src/colors";
+import { EventColors } from "../../Types/Events";
+import { colors } from "../../src/colors";
 
-export function pickColor(number_of_guests: number) {
+export function pickColor(
+  number_of_guests: number,
+  partyMarkerColors: EventColors
+) {
   if (number_of_guests <= 10) {
-    return colors.partyMarkerColors.smallGroupOfPeople;
+    return partyMarkerColors.intimateGathering;
   } else if (number_of_guests >= 10 && number_of_guests <= 20) {
-    return colors.partyMarkerColors.midGroupOfPeople;
+    return partyMarkerColors.mediumGathering;
   } else {
-    return colors.partyMarkerColors.bigGroupOfPeople;
+    return partyMarkerColors.largeGathering;
   }
 }
