0faf0e72c9525e13ab505567a76a1e439df17179
aswds <sasha.iilllnitskiy@gmail.com>
2023-05-15 20:21:18 +0300

    🚀 feat(References.ts): rename RSVP_Types to Party_Access_Types for better readability and add eventDocToListenReference function to listen to events in a city.

--- app/Firebase/References.ts
+++ app/Firebase/References.ts
@@ -1,12 +1,14 @@
 import { collection, doc } from "firebase/firestore";
 import { db } from "../../firebase";
-import { RSVP_Types } from "../Types/Events";
+import { Party_Access_Types } from "../Types/Events";
 
-export const eventsReference = (userLocation: string, eventType: RSVP_Types) =>
-  collection(db, "EVENTS", `${userLocation}`, `${eventType}`);
+export const eventsReference = (
+  userLocation: string,
+  eventType: Party_Access_Types
+) => collection(db, "EVENTS", `${userLocation}`, `${eventType}`);
 export const eventReference = (
   userLocation: string,
-  eventType: RSVP_Types,
+  eventType: Party_Access_Types,
   path: string
 ) => doc(db, "EVENTS", `${userLocation}`, eventType, path);
 
@@ -14,3 +16,6 @@ export const userReference = (userUID: string) =>
   doc(db, `USERS`, `${userUID}`);
 export const userNotificationReference = (userUID: string) =>
   collection(db, `USERS`, `${userUID}`);
+
+export const eventDocToListenReference = (city: string) =>
+  doc(db, `EVENTS`, `${city}`);
