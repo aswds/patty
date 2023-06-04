f500c1e8f535f5b90eb7b646b8d461121ed1c542
aswds <sasha.iilllnitskiy@gmail.com>
2023-06-05 01:02:52 +0300

    [refactor] Change screen location

--- app/navigation/Navigators/MapStack.tsx
+++ app/navigation/Navigators/MapStack.tsx
@@ -5,19 +5,16 @@ import { MapStackNavigatorParamList } from "../../Types/MapStack/NavigationTypes
 import { PartyCreationStack } from "./PartyCreationStack";
 import Guests from "../../screens/Modals/PartyModal/screen/Guests";
 import PartyScreen from "../../screens/Party/PartyScreen";
+import Invitation from "../../screens/Modals/PartyModal/screen/Invitation";
 
 const Stack = createNativeStackNavigator<MapStackNavigatorParamList>();
 export const MapNavigator = () => {
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
       <Stack.Screen component={Map} name="Map" />
-      <Stack.Screen
-        component={PartyCreationStack}
-        name="PartyCreationStack"
-        options={{ gestureEnabled: false }}
-      />
+      <Stack.Screen component={PartyCreationStack} name="PartyCreationStack" />
       <Stack.Screen component={Guests} name={"Guests"} options={{}} />
-
+      <Stack.Screen component={Invitation} name={"InvitationScreen"} />
       {/*<Stack.Screen component={AddCreatorsScreen} name="AddCreators" />*/}
     </Stack.Navigator>
   );
--- app/navigation/Navigators/PartyStack.tsx
+++ app/navigation/Navigators/PartyStack.tsx
@@ -7,12 +7,24 @@ import Guests from "../../screens/Modals/PartyModal/screen/Guests";
 import { PartyNavigatorParamList } from "../../Types/PartyStack/NavigationTypes";
 import { NavigationContainer } from "@react-navigation/native";
 import PartyScreen from "../../screens/Party/PartyScreen";
+import PostUploadScreen from "../../screens/Party/components/AddPost/UploadPostScreen";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Button from "../../shared/Buttons/Button";
 
 const Stack = createNativeStackNavigator<PartyNavigatorParamList>();
 export const PartyNavigator = () => {
   return (
-    <Stack.Navigator screenOptions={{ headerShown: false }}>
+    <Stack.Navigator
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
       <Stack.Screen component={PartyScreen} name="PartyScreen" />
+      <Stack.Screen
+        component={PostUploadScreen}
+        name="PostUploadScreen"
+        options={{}}
+      />
       {/*<Stack.Screen component={AddCreatorsScreen} name="AddCreators" />*/}
     </Stack.Navigator>
   );
