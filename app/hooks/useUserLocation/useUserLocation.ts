b65b2da0bc02be26a6099bb811ba19cb8d686ee6
aswds <sasha.iilllnitskiy@gmail.com>
2023-06-05 00:59:53 +0300

    Importing the necessary dependencies, including expo-location and firebase/firestore. Defining the UserLocation interface to represent the user's location and city. Creating the useUserLocation hook as a function component. Setting up state variables such as location, isLoading, and setLocation. Implementing the useEffect hook to handle the location fetching logic. Requesting foreground location permissions using Location.requestForegroundPermissionsAsync(). Retrieving the user's current position using Location.getCurrentPositionAsync(). Reverse geocoding the coordinates to obtain the city using Location.reverseGeocodeAsync(). Updating the location state with the retrieved latitude, longitude, and city. Handling errors and setting the isLoading state accordingly.

--- app/hooks/useUserLocation/useUserLocation.ts
+++ app/hooks/useUserLocation/useUserLocation.ts
@@ -1,16 +1,9 @@
-import { UserLocation } from "./../../Types/User";
+import * as Location from "expo-location";
+import { getDocs } from "firebase/firestore";
 import { useEffect, useState } from "react";
-import { getUserLocation } from "../../shared/GetLocationFunctions/getUserLocation";
-import { getDocs, onSnapshot } from "firebase/firestore";
-import { getAddress } from "../../shared/GetLocationFunctions/getAddress";
-import { ICoordinates, IEvent, IFullAddress } from "../../Types/Events";
-import { LocationObject } from "expo-location";
 import { Alert } from "react-native";
 import { eventsReference } from "../../Firebase/References";
-import { useActions } from "../useActions";
-import { useAppDispatch } from "../useAppDispatch";
-import { updateUserLocation } from "../../redux/reducers/User";
-
+import { IEvent } from "../../Types/Events";
 /**
  * Get all parties in user's city
  * @param userLocation
@@ -40,49 +33,47 @@ export async function fetchCityParties(
   });
 }
 
-export default function useUserLocation() {
-  // setUserLocation?: Dispatch<SetStateAction<ICoordinates | undefined>>
-  const [userLocation, setUserLocation] = useState<ICoordinates>();
-  const [errorMsg, setErrorMsg] = useState<string>();
-  const [city, setCity] = useState<string>();
-  const [isLocationLoading, setIsLoading] = useState<boolean>(true);
-  const dispatch = useAppDispatch();
-  useEffect(() => {
-    async function fetchData() {
-      try {
-        setIsLoading(true);
-        await getUserLocation().then(async (res: LocationObject) => {
-          setUserLocation({
-            latitude: res.coords.latitude,
-            latitudeDelta: 0,
-            longitude: res.coords.longitude,
-            longitudeDelta: -0.01,
-          });
+interface UserLocation {
+  coords: {
+    latitude: number;
+    longitude: number;
+  };
+  city: string | undefined;
+}
 
-          await getAddress(res.coords.latitude, res.coords.longitude).then(
-            (r: IFullAddress) => {
-              setCity(r?.city);
-              dispatch(
-                updateUserLocation({
-                  city: r?.city,
-                  location: {
-                    latitude: res.coords.latitude,
-                    latitudeDelta: 0,
-                    longitude: res.coords.longitude,
-                    longitudeDelta: -0.01,
-                  },
-                })
-              );
-              setIsLoading(false);
-            }
-          );
+export const useUserLocation = (): [UserLocation, boolean] => {
+  const [location, setLocation] = useState<UserLocation>({
+    coords: { latitude: 0, longitude: 0 },
+    city: undefined,
+  });
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const { status } = await Location.requestForegroundPermissionsAsync();
+        if (status !== "granted") {
+          throw new Error("Location permission not granted");
+        }
+
+        const { coords } = await Location.getCurrentPositionAsync({});
+        const { latitude, longitude } = coords;
+
+        setLocation({ ...location, coords: { latitude, longitude } });
+
+        const address = await Location.reverseGeocodeAsync({
+          latitude,
+          longitude,
         });
-      } catch (e: any) {
-        setErrorMsg(e);
+        const city = address.length > 0 ? address[0].subregion : undefined;
+
+        setLocation((prevLocation) => ({ ...prevLocation, city: city! }));
+        setIsLoading(false);
+      } catch (error) {
+        setIsLoading(false);
       }
-    }
-    fetchData();
+    })();
   }, []);
 
-  return { userLocation, city, errorMsg, isLocationLoading };
-}
+  return [location, isLoading];
+};
