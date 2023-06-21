6ffff4330967e1e41af03200c27af99005d0f6a3
aswds <sasha.iilllnitskiy@gmail.com>
2023-06-21 13:00:56 +0300

    [style]- useFontsLoad.ts changed main font

--- app/hooks/useFontsLoad.ts
+++ app/hooks/useFontsLoad.ts
@@ -3,20 +3,16 @@ import { useFonts } from "expo-font";
 export const useFontsLoad = () => {
   const [isLoaded, error] = useFonts({
     "Lato-Regular": require("../../assets/fonts/Lato/Lato-Regular.ttf"),
-    "Lato-Bold": require("../../assets/fonts/Lato/Lato-Bold.ttf"),
-    "Poppins-Bold": require("../../assets/fonts/Poppins/Poppins-Bold.ttf"),
-    "Poppins-Regular": require("../../assets/fonts/Poppins/Poppins-Regular.ttf"),
-    "Roboto-Bold": require("../../assets/fonts/Roboto/Roboto-Bold.ttf"),
-    "Roboto-Regular": require("../../assets/fonts/Roboto/Roboto-Regular.ttf"),
-    "WorkSans-Bold": require("../../assets/fonts/WorkSans/WorkSans-Bold.ttf"),
-    "WorkSans-Regular": require("../../assets/fonts/WorkSans/WorkSans-Regular.ttf"),
-    "WorkSans-Medium": require("../../assets/fonts/WorkSans/WorkSans-Medium.ttf"),
-    "WorkSans-SemiBold": require("../../assets/fonts/WorkSans/WorkSans-SemiBold.ttf"),
     "Nunito-ExtraBold": require("../../assets/fonts/Nunito/static/Nunito-ExtraBold.ttf"),
     "Nunito-Bold": require("../../assets/fonts/Nunito/static/Nunito-Bold.ttf"),
     "Nunito-SemiBold": require("../../assets/fonts/Nunito/static/Nunito-SemiBold.ttf"),
     "Nunito-Medium": require("../../assets/fonts/Nunito/static/Nunito-Medium.ttf"),
     "Nunito-Regular": require("../../assets/fonts/Nunito/static/Nunito-Regular.ttf"),
+    "Baloo2-Regular": require("../../assets/fonts/Baloo2/Baloo2-Regular.ttf"),
+    "Baloo2-SemiBold": require("../../assets/fonts/Baloo2/Baloo2-SemiBold.ttf"),
+    "Baloo2-ExtraBold": require("../../assets/fonts/Baloo2/Baloo2-ExtraBold.ttf"),
+    "Baloo2-Medium": require("../../assets/fonts/Baloo2/Baloo2-Medium.ttf"),
+    "Baloo2-Bold": require("../../assets/fonts/Baloo2/Baloo2-Bold.ttf"),
   });
 
   return { isLoaded, error };
