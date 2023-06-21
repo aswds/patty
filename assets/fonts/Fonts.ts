7b9e5382bb3a49891acdaa0ebca11e652a23ecaa
aswds <sasha.iilllnitskiy@gmail.com>
2023-06-21 13:02:39 +0300

    [chore] - delete unnecessary fonts, add main font

--- assets/fonts/Fonts.ts
+++ assets/fonts/Fonts.ts
@@ -1,13 +1,11 @@
 import { IFonts } from "../../app/Types/Fonts/FontTypes";
 
 const FontFamily: IFonts = {
-  bold: "Nunito-Bold",
-  regular: "Nunito-Regular",
-
-  medium: "Nunito-Medium",
-
-  semi_bold: "Nunito-SemiBold",
-  extra_bold: "Nunito-ExtraBold",
+  bold: "Baloo2-Bold",
+  regular: "Baloo2-Regular",
+  medium: "Baloo2-Medium",
+  semi_bold: "Baloo2-SemiBold",
+  extra_bold: "Baloo2-ExtraBold",
 };
 
 export { FontFamily };
