b9925332ad9cebc061ed0c2d0ebff59f71e8028b
aswds <sasha.iilllnitskiy@gmail.com>
2023-06-21 13:10:11 +0300

    [feat] - add new screen to pick media

--- app/navigation/Navigators/PartyStack.tsx
+++ app/navigation/Navigators/PartyStack.tsx
@@ -1,16 +1,9 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import React from "react";
-import Map from "../../screens/Map/Map";
-import { MapStackNavigatorParamList } from "../../Types/MapStack/NavigationTypes";
-import { PartyCreationStack } from "./PartyCreationStack";
-import Guests from "../../screens/Modals/PartyModal/screen/Guests";
 import { PartyNavigatorParamList } from "../../Types/PartyStack/NavigationTypes";
-import { NavigationContainer } from "@react-navigation/native";
 import PartyScreen from "../../screens/Party/PartyScreen";
+import AddAnnouncementScreen from "../../screens/Party/components/AddAnnouncement/AddAnnouncementScreen";
+import MediaListToUpload from "../../screens/Party/components/AddPost/MediaListToUpload";
 import PostUploadScreen from "../../screens/Party/components/AddPost/UploadPostScreen";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import Button from "../../shared/Buttons/Button";
-
 const Stack = createNativeStackNavigator<PartyNavigatorParamList>();
 export const PartyNavigator = () => {
   return (
@@ -24,6 +17,11 @@ export const PartyNavigator = () => {
         component={PostUploadScreen}
         name="PostUploadScreen"
         options={{}}
+      />
+      <Stack.Screen component={MediaListToUpload} name="MediaListToUpload" />
+      <Stack.Screen
+        component={AddAnnouncementScreen}
+        name="AddAnnouncementScreen"
       />
       {/*<Stack.Screen component={AddCreatorsScreen} name="AddCreators" />*/}
     </Stack.Navigator>
